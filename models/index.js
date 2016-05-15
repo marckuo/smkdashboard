ab7b2225e11b0142f010390dcb9d2f9894e52b65
Ksenia <gueletina.ksenia@gmail.com>
2016-05-14 22:00:50 -0400

    heroku database settings - took out db details because sequelize should parse them

--- models/index.js
+++ models/index.js
@@ -12,13 +12,7 @@ if (!global.hasOwnProperty('db')) {
 
   if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
-    })
+    sequelize = new Sequelize(process.env.DATABASE_URL)
   } else {
     // the application is executed on the local machine ... use mysql
     sequelize = new Sequelize(config.database, config.username, config.password, config);
