b2ddd58b44fdd8b373d951b2408e594480e0e460
Ksenia <gueletina.ksenia@gmail.com>
2016-05-18 17:49:03 -0400

    audio visualization working

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,45 +1,20 @@
 var socket;
-var lastTimeSent;
-var now;
-var vol;
+var fft;
+var binCount = 512;
+var bins = new Array(binCount);
+
 function setup() {
-  // Create an Audio input
   mic = new p5.AudioIn();
 
-  //connects a socket to the page URL
-  //socket = io(window.location.origin);
-  console.log(socket);
-  // start the Audio Input.
-  // By default, it does not .connect() (to the computer speakers)
-  mic.start();
-  frameRate(15);
+  var smoothing = 0.6;
+  fft = new p5.FFT(smoothing, binCount);
 
+  frameRate(10);
+  mic.start();
+  fft.setInput(mic);
 }
 
 function draw() {
-  now = new Date();
-  vol = mic.getLevel();
-
-  // if(lastTimeSent === undefined || now - lastTimeSent > 500){
-  //   sendSound();
-  //   console.log('sent');
-  //   lastTimeSent = now;
-  //   socket.emit('sound', vol);
-  // }
-  //vol stores the stream of volume information
-  console.log('vol: ' + vol);
-
-  //sends volume stream to the server, which is then sent to index.ejs
-  // setTimeout(function(){
-    socket.emit('sound', vol);
-  //   console.log('message sent')
-  // }, 10000)
+  var spectrum = fft.analyze();
+  socket.emit('sound', spectrum);
 }
-//
-// var sendSound = function(){
-//   if(lastTimeSent === undefined || now - lastTimeSent > 500){
-//     socket.emit('sound', vol);
-//     console.log(vol);
-//     lastTimeSent = now;
-//   }
-// }
