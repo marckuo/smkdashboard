1a7069c47e6aabfdc37b4a9fddac7c9ee77a01ff
Ksenia <gueletina.ksenia@gmail.com>
2016-05-22 23:10:07 -0400

    /last/beverage now returns an array of dates for the last day

--- routes/api.js
+++ routes/api.js
@@ -1,57 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var models  = require('../models');
+var getModel = require('../helpers/getSensorModel');
 
 router.get('/last/:sensor_name', function(req, res) {
   var today = new Date();
   var endDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
   var startDate = new Date(endDate - 24 * 60 * 60 * 1000);
   var sensor_name = req.params.sensor_name;
-
-  switch(sensor_name) {
-    case 'temp':
-      models.Temp.findOne(
-        { order: 'id DESC' }
-      )
-      .then(function(temp, metadata) {
-        res.json(temp);
-      });
-      break;
-    case 'humid':
-      models.Humid.findOne(
-        { order: 'id DESC' }
-      )
-      .then(function(humid, metadata) {
-        res.json(humid);
-      });
-      break;
-    case 'door':
-      models.Door.count({
-        where: {
-          createdAt: {
-            $gt: startDate,
-            $lt: endDate
-          }
-        }
-      })
-      .then(function(door_num, metadata){
-        res.json(door_num);
-      });
-      break;
-    case 'beverage':
-      models.Beverage.count({
-        where: {
-          createdAt: {
-            $gt: startDate,
-            $lt: endDate
-          }
-        }
-      })
-      .then(function(beverage_num, metadata){
-        res.json(beverage_num);
-      });
-      break;
-  }
+  var time = req.params.time;
+  var model = getModel(sensor_name);
+  console.log(model);
+  console.log('-----------test----------');
+  //////////////////////////////////////////////////////
+  model.scope('last_day')
+    .findAll()
+    .then(function(result){
+      res.json(result)
+    });
+  //////////////////////////////////////////////////////
+  // console.log(model.last_day())
+  // res.json(model.last_day());
+  console.log('-----------test----------');
+  //
+  // if(time === undefined && model !== undefined){
+  //   if(sensor_name === 'temp' || sensor_name === 'humid'){
+  //     model.findOne(
+  //       { order: 'id DESC' }
+  //     )
+  //     .then(function(temp, metadata) {
+  //       res.json(temp);
+  //     });
+  //   } else if (sensor_name === 'door' || sensor_name === 'beverage') {
+  //     model.count({
+  //       where: {
+  //         createdAt: {
+  //           $gt: startDate,
+  //           $lt: endDate
+  //         }
+  //       }
+  //     })
+  //     .then(function(door_num, metadata){
+  //       res.json(door_num);
+  //     });
+  //   }
+  // }
 });
 
 router.get('/all/:sensor_name', function(req, res) {
